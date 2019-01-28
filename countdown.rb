419cb0d756b6a689f2f2f746efc01ea61df45bdc
Daniel Shimield <dxshim90@gmail.com>
2019-01-28 17:19:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
-
-def countdown
+def countdown(x)
+  while x <= 10
+  puts "#{x} SECOND(S)!"
+  x -= 1
 end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown(x)
+  while x <= 10
+  sleep (3)
+  puts "#{x} SECOND(S)!"
+  x -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+
