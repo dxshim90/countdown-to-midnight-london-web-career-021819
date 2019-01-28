a2ecfc6275b382b626ed69b7f1dc6b57d6c5c509
Daniel Shimield <dxshim90@gmail.com>
2019-01-28 17:20:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,5 @@
 def countdown(x)
-  while x <= 10
+  while x > 0
   puts "#{x} SECOND(S)!"
   x -= 1
 end
@@ -7,7 +7,7 @@ return "HAPPY NEW YEAR!"
 end
 
 def countdown(x)
-  while x <= 10
+  while x > 0
   sleep (3)
   puts "#{x} SECOND(S)!"
   x -= 1
